2e3b0584f0fc29efc8d883767a9ccc28bdfcf621
James Chen <jamesc@uniteaudio.com>
2026-09-22 11:21:58 +1000

    Add micro-architecture SysTick example

---
+++ embedded-rust/ch2-app2/src/bin/micro-arch.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use cortex_m::peripheral::{Peripherals, syst};
+use cortex_m_rt::entry;
+
+use defmt_rtt as _;
+use panic_probe as _;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let mut systick = peripherals.SYST;
+    systick.set_clock_source(syst::SystClkSource::Core);
+    systick.set_reload(1_000);
+    systick.clear_current();
+    systick.enable_counter();
+    while !systick.has_wrapped() {
+        // Loop
+    }
+
+    loop {}
+}
